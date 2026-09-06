31d8dfeacb4fbe2f43ac514e9b29fcb98650dce4
bhabeshHenasaha227 <bhabeshhenasaha227@gmail.com>
2026-09-06 10:53:17 +0530

    all the thing getting changed

--- src/components/arrayandobject/ArrObj.tsx
+++ src/components/arrayandobject/ArrObj.tsx
@@ -1,12 +1,39 @@
 export default function ArrObj() {
-  const fruites: string[] = ["apple", "banana", "mango", "orange"];
+  type user = {
+    name: string;
+    age: number;
+    passion: string;
+  };
+  // const fruites: string[] = ["apple", "banana", "mango", "orange"];
+  const users: user[] = [
+    { name: "Bhabesh", age: 40, passion: "coding" },
+    { name: "salman", age: 20, passion: "coding" },
+    { name: "Vijay", age: 30, passion: "coding" },
+  ];
   return (
     <>
-      <ul>
-        {fruites.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
-        ))}
-      </ul>
+      <div>
+        {/* <ul>
+          {fruites.map((fruit) => (
+            <li key={fruit}>{fruit}</li>
+          ))}
+        </ul> */}
+      </div>
+      <br />
+      <div>Object looping</div>
+      <br />
+      <div>
+        <ul>
+          {users.map((user: user, index: number) => (
+            <li key={user.name}>
+              <span>{index + 1}</span>
+              <div>{user.name}</div>
+              <div>{user.age}</div>
+              <div>{user.passion}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  );
+  )
 }
